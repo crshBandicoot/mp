4960c5c0a072164a0565ec9251f6a58ce3f32a2f
Bogdan Pizhuk <bogdanpizuk2604@gmail.com>
2022-06-17 23:00:32 +0300

    +format

--- src/main/scala/Lst.scala
+++ src/main/scala/Lst.scala
@@ -35,22 +35,15 @@ enum Lst[+A]:
     rec(this)
 
   def split: (Lst[A], Lst[A]) =
-    def rec[A](
-        list: Lst[A] = this,
-        mid: Double = floor(this.len / 2),
-        n: Int = 0,
-        left: Lst[A] = Nil,
-        right: Lst[A] = Nil
-    ): (Lst[A], Lst[A]) =
-      if n < mid then {
+    def rec[A](list: Lst[A] = this, mid: Double = floor(this.len / 2), cnt: Int = 0, left: Lst[A] = Nil, right: Lst[A] = Nil): (Lst[A], Lst[A]) =
+      if cnt < mid then {
         list match
           case Nil             => (left.reverse, right.reverse)
-          case Cns(head, tail) => rec(tail, mid, n + 1, Cns(head, left), right)
-      } else {
+          case Cns(head, tail) => rec(tail, mid, cnt + 1, Cns(head, left), right)}
+          else {
         list match
           case Nil             => (left.reverse, right.reverse)
-          case Cns(head, tail) => rec(tail, mid, n + 1, left, Cns(head, right))
-      }
+          case Cns(head, tail) => rec(tail, mid, cnt + 1, left, Cns(head, right))}
     rec()
 
   def getHead: A =
@@ -65,10 +58,7 @@ enum Lst[+A]:
         case Cns(head, tail) =>
           if inits != Nil then rec(tail, Cns(Cns(head, inits.getHead), inits))
           else rec(tail, Cns(Cns(head, Nil), inits))
-    def rev(
-        list: Lst[Lst[A]] = rec(this),
-        reversed: Lst[Lst[A]] = Nil
-    ): Lst[Lst[A]] =
+    def rev(list: Lst[Lst[A]] = rec(this), reversed: Lst[Lst[A]] = Nil): Lst[Lst[A]] =
       list match
         case Nil             => reversed
         case Cns(head, tail) => rev(tail, Cns(head.reverse, reversed))
@@ -76,26 +66,15 @@ enum Lst[+A]:
       case Nil => Lst(Lst())
       case _   => rev()
 
-  def exists[A](
-      predicate: (A) => Boolean,
-      mylist: Lst[A] = this
-  ): Boolean =
-    def rec(
-        func: (A) => Boolean,
-        list: Lst[A] = mylist
-    ): Boolean =
+  def exists[A](predicate: (A) => Boolean, mylist: Lst[A] = this): Boolean =
+    def rec(func: (A) => Boolean, list: Lst[A] = mylist): Boolean =
       list match
         case Nil => false
-        case Cns(head, tail) =>
-          if func(head) then true else rec(func, tail)
+        case Cns(head, tail) => if func(head) then true else rec(func, tail)
     rec(predicate)
 
   def merge[A](l: Lst[A], r: Lst[A], comparator: (A, A) => Compared): Lst[A] =
-    def rec(
-        left: Lst[A],
-        right: Lst[A],
-        comp: (A, A) => Compared
-    ): Lst[A] =
+    def rec(left: Lst[A], right: Lst[A], comp: (A, A) => Compared): Lst[A] =
       (left, right) match
         case (left, Nil)  => left
         case (Nil, right) => right
@@ -105,19 +84,13 @@ enum Lst[+A]:
           else Cns(rightHead, merge(left, rightTail, comp))
     rec(l,r, comparator)
 
-  def mergeSort[A](
-      comparator: (A, A) => Compared,
-      mylist: Lst[A] = this
-  ): Lst[A] =
+  def mergeSort[A](comparator: (A, A) => Compared, mylist: Lst[A] = this): Lst[A] =
     def rec(comp: (A, A) => Compared, list: Lst[A] = mylist): Lst[A] =
       if list.len == 0 || list.len == 1 then list
-      else {
-        val (left, right) = list.split
-        merge(rec(comp, left), rec(comp, right), comp)
-      }
+      else {val (left, right) = list.split
+            merge(rec(comp, left), rec(comp, right), comp)}
     rec(comparator)
 
 object Lst {
   def apply[A](list: A*): Lst[A] =
-    list.foldRight(Nil: Lst[A]) { case (head, tail) => Cns(head, tail) }
-}
+    list.foldRight(Nil: Lst[A]) { case (head, tail) => Cns(head, tail) }}
